7bbab78e7693b9469691b67a38c6d017ccabf642
mohmdloai <mhmd.00.eng@gmail.com>
2025-09-27 19:38:53 +0300

    fix functins, and use them in a use case

--- crypto_01.js
+++ crypto_01.js
@@ -14,20 +14,31 @@ Scenarios of  CryptoJS use case;
 
 import CryptoJs from "crypto-js";
 const encryptText = (passedText, key) => {
-  const cipherText = CryptoJs.AES(passedText, key).toString();
+  const cipherText = CryptoJs.AES.encrypt(passedText, key).toString();
   //binary to ascii with base 64 encoding ;
   return btoa(cipherText);
 };
 
-const dycrptText = (cipherTextOfBase64, key) => {
+const decryptText = (cipherTextOfBase64, key) => {
   try {
     //  -> atob
     const cipherText = atob(cipherTextOfBase64);
-    const bytes = Crypto.AES.dycrptText(cipherText, key);
+    const bytes = CryptoJs.AES.decrypt(cipherText, key);
     const originalText = bytes.toString(CryptoJs.enc.Utf8);
     return originalText;
   } catch (error) {
     console.error(error);
-    return "Dycrption failed";
+    return "Decryption failed";
   }
 };
+
+/* key to use is today's date */
+
+const userMeta = (userName, userId) => userName + userId;
+
+const todayDate = new Date().toISOString().split("T")[0];
+const token = encryptText(userMeta("mhmd", 443), todayDate);
+console.log("Encrypted Token: ", token);
+
+const decrypted = decryptText(token, todayDate);
+console.log("Decrypted Token: ", decrypted);
