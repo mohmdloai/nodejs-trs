52e43c62e3a650b7cee14e79ddea3761e7bf2880
mohmdloai <mhmd.00.eng@gmail.com>
2025-12-27 20:43:20 +0200

    Add debug logging and sample Locations to user

--- partialType.js
+++ partialType.js
@@ -2,8 +2,10 @@ const makePartial= (obj) => {
   if (obj === null || typeof obj !== 'object') return obj;
   for (const k of Object.keys(obj)) {
     obj[k] = obj[k] ?? {}; // Make each key optional
+    console.log(obj[k]); // Log the value of each key
     if (typeof obj[k] === 'object') {
       makePartial(obj[k]);
+      // console.log(obj[k]); // Log the value of each key after recursion
     }
   }
   return obj;
@@ -14,6 +16,10 @@ const user = {
   address: {
     street: "123 El3sloggey St",
     city: "El3sloggey"
+  },
+  Locations: {
+    club: "Zag club ",
+    membership: "member of Zag club"
   }
 };
 const partialUser = makePartial(user);
